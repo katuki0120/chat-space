3024be96c8378c4fa953920434fb1f4dc284fff6
katuki0120 <katuki2898@gmail.com>
2020-04-29 19:28:04 +0900

    デプロイの自動化　＃7?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-server '52.69.130.151', user: 'ec2-user', roles: %w{app db web}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
